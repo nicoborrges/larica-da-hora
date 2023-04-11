4bf9bd7781c1467865114fd6176f7ef729ea7445
Nicolas Josep Silveira Borges <111542614+nicoborrges@users.noreply.github.com>
2023-04-11 18:18:25 -0300

    Revert "Add gitignore and .env.example"
    This reverts commit 5c25e3c3d62e4284a41d5709ea9d707517fd5451.

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import {View, Text} from 'react-native'
 import { Routes } from './src/routes'
 import { NavigationContainer } from '@react-navigation/native'
 
-require('dotenv').config()
-
 export default function App(){
   return (
       <NavigationContainer>
