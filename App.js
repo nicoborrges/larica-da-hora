5c25e3c3d62e4284a41d5709ea9d707517fd5451
Nicolas Josep Silveira Borges <111542614+nicoborrges@users.noreply.github.com>
2023-04-11 15:46:50 -0300

    Add gitignore and .env.example

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import {View, Text} from 'react-native'
 import { Routes } from './src/routes'
 import { NavigationContainer } from '@react-navigation/native'
 
+require('dotenv').config()
+
 export default function App(){
   return (
       <NavigationContainer>
